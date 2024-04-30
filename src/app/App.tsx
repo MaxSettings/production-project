4c1d7ec720af0e27cb31b4471df476d66eda4c95
sergey.koval <sergey.koval@hrb.software>
2024-04-30 23:02:47 +0300

    добавил errorboundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -8,6 +8,10 @@ import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
   const { theme } = useTheme();
+
+  useEffect(() => {
+    throw new Error();
+  }, []);
 
   return (
     <div className={classNames('app', {}, [theme])}>
